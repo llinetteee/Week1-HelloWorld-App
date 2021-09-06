04472e6cf21cd957e75200d1ba9310282721cf2c
Linette Listmann <linette@Linettes-Air.home>
2021-09-06 19:41:51 +0300

    Create Hello World Program

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            string Username;
+            Username = Console.ReadLine();
+
+            Console.WriteLine("Hello, " + Username);
+
+            Console.Read();
+        }
+    }
+}
